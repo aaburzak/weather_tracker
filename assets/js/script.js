7d7b6044aa61bcb41ee5410415ea151e7c8d186d
Aaron Burzak <aaron.j.burzak@gmail.com>
2022-02-04 12:44:12 -0500

    adjusted styling for current day and 5day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,6 +54,7 @@ function citySearch(cityName){
       $("#uvIndex").text("UV Index: " + uv);
 
       //variables and logic to render 5 day forecast
+      $("#nextFive").text("Next Five Days")
       var iconDay1 = weatherReport.daily[1].weather[0].icon;
       var iconDay2 = weatherReport.daily[2].weather[0].icon;
       var iconDay3 = weatherReport.daily[3].weather[0].icon;
@@ -64,6 +65,39 @@ function citySearch(cityName){
       $("#futureIcon3").html(`<img src="http://openweathermap.org/img/wn/${iconDay3}@2x.png">`);
       $("#futureIcon4").html(`<img src="http://openweathermap.org/img/wn/${iconDay4}@2x.png">`);
       $("#futureIcon5").html(`<img src="http://openweathermap.org/img/wn/${iconDay5}@2x.png">`);
+
+      var tempDay1 = weatherReport.daily[1].temp.day;
+      var tempDay2 = weatherReport.daily[2].temp.day;
+      var tempDay3 = weatherReport.daily[3].temp.day;
+      var tempDay4 = weatherReport.daily[4].temp.day;
+      var tempDay5 = weatherReport.daily[5].temp.day;
+      $("#futureTemp1").text(tempDay1+"°F");
+      $("#futureTemp2").text(tempDay2+"°F");
+      $("#futureTemp3").text(tempDay3+"°F");
+      $("#futureTemp4").text(tempDay4+"°F");
+      $("#futureTemp5").text(tempDay5+"°F");
+
+      var humidityDay1 = weatherReport.daily[1].humidity;
+      var humidityDay2 = weatherReport.daily[2].humidity;
+      var humidityDay3 = weatherReport.daily[3].humidity;
+      var humidityDay4 = weatherReport.daily[4].humidity;
+      var humidityDay5 = weatherReport.daily[5].humidity;
+      $("#futureHumid1").text("Humidity: " +humidityDay1+"%");
+      $("#futureHumid2").text("Humidity: " +humidityDay2+"%");
+      $("#futureHumid3").text("Humidity: " +humidityDay3+"%");
+      $("#futureHumid4").text("Humidity: " +humidityDay4+"%");
+      $("#futureHumid5").text("Humidity: " +humidityDay5+"%");
+
+      var windDay1 = weatherReport.daily[1].wind_speed;
+      var windDay2 = weatherReport.daily[2].wind_speed;
+      var windDay3 = weatherReport.daily[3].wind_speed;
+      var windDay4 = weatherReport.daily[4].wind_speed;
+      var windDay5 = weatherReport.daily[5].wind_speed;
+      $("#futureWind1").text("Wind Speed: " + windDay1+"mph");
+      $("#futureWind2").text("Wind Speed: " + windDay2+"mph");
+      $("#futureWind3").text("Wind Speed: " + windDay3+"mph");
+      $("#futureWind4").text("Wind Speed: " + windDay4+"mph");
+      $("#futureWind5").text("Wind Speed: " + windDay5+"mph");
     })
   })
 }
